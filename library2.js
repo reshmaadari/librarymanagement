b1a18205a0944edf87858130c62cba0cc76f879c
reshmaadari <93297201+reshmaadari@users.noreply.github.com>
2021-11-01 17:26:16 +0530

    Update library2.js

--- library2.js
+++ library2.js
@@ -1,46 +1,24 @@
-// var books = [{}];
-// var i=0;
+
+ var i=0;
  function bookEntry(tbname,tbauthor)
  {
-  books.push({id: i,textbookname:tbname,author:tbauthor});
+  books.push({"id": i,"textbookname":tbname,"author":tbauthor});
   i++;
   window.alert("book Added to List");
 }
 function bookassign(){
-  for (var k=0;k<i;k++){
-    window.alert(k+"."+books[k].tbauthor+"-"+books[k].author);
-    window.alert(k+"."+books[k].tbname+"-"+books[k].textbookname);
+  for (var k=0;k<books.length;k++){
+    window.alert(books[k]["id"]+books[k]["textbookname"]);
+    window.alert(books[k]["id"]+books[k]["author"]);
   }
-  var wbn=readlineSync.question("Enter Book Number You Need");
+  var wbn=prompt("Enter Book Number You Need");
   delete books[wbn];
   i--;
   window.alert("Book has been Assigned to YOU");
 }
 function showbook(){
-  for (var k=0;k<i;k++){
-    window.alert(k+"."+books[k].tbauthor+"-"+books[k].author);
-    window.alert(k+"."+books[k].tbname+"-"+books[k].textbookname);
+  for (var k=0;k<books.length;k++){
+    window.alert(books[k]["id"]+books[k]["textbookname"]);
+    window.alert(books[k]["id"]+books[k]["author"]);
   }
 }
-// window.alert("1.New Book Entry \n 2.Take Book \n 3.balance \n 4.exit");
-
-// var choice=readlineSync.question("enter your choice you want:");
-// if (choice == "1"){
-//     window.alert("Welcome to the New Book Entry");
-//   let tbname=readlineSync.question("Enter TextBook Name");
-//   let tbauthor=readlineSync.question("Enter TextBook Author Name");
-//   bookEntry(tbname,tbauthor);
-//   break;
-// }
-//   else if (choice == "2"){
-//     bookassign();
-//   }
-//   else if (choice == "3"){
-//     showbooks();
-//   }
-//   else if (choice == "4"){
-//     break;
-//   }
-//   else{
-//     window.alert("Enter Your Choice From List shown");
-//   }
